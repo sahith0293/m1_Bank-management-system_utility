f92564c9a152a56f83bc2f93af4593f651adb4d3
sahith0293 <63460090+sahith0293@users.noreply.github.com>
2022-02-06 22:07:20 +0530

    app

---
+++ aple.c
@@ -0,0 +1,17 @@
+
+/**
+ * @file aple.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-04
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello sahith");
+    return 0;
+}
